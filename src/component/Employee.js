cfd44a4d68e0a3470a4992d8704b2cce5a2b221e
Prajeeth28 <prajeeth06@gmail.com>
2024-08-06 14:13:09 +0530

    updated crud app

---
+++ src/component/Employee.js
@@ -0,0 +1,19 @@
+const Employee = [
+    {
+        id: "1",
+        Name: "Ravi",
+        Age: "24"
+    },
+    {
+        id: "2",
+        Name: "Kamal",
+        Age: "25"
+    },
+    {
+        id: "3",
+        Name: "Jeeva",
+        Age: "27"
+    }
+]
+
+export default Employee;
